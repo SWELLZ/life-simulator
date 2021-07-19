8fd20b2d5a249082964926977447471e2d6be2b5
SWELLZ <microsoftdavid4@gmail.com>
2021-07-19 09:37:59 -0700

    Add More Randomized Events
    I made arrays holding random events, description and button texts. NOT EFFICIENT but it works for me.

--- Assets/Scripts/RandomEvent.cs
+++ Assets/Scripts/RandomEvent.cs
@@ -13,12 +13,21 @@ public class RandomEvent : MonoBehaviour
     public Button choice2;
     public Text choice2Txt;
 
+    public string[] randomEventTitles = {"First Kiss", "Free Money", "Job Offer"};
+    public string[] randomEventDescs = { "A girl tries to kiss you. It will be your first kiss!",
+    "You find a briefcase with $1,000 in it!",
+    "A man offers you a job to Detail a rich persons car for $12,000 a year"
+    };
+    public string[] choice1Texts = {"Kiss her", "Take It", "Accept the Job"};
+    public string[] choice2Texts = {"Run Away", "Leave It", "Decline the Job"};
+
     public void RandomScenario()
     {
-        titleText.text = "First Kiss!";
-        descriptionText.text = "A girl tries to kiss you. It will be your first kiss!";
-        choice1Txt.text = "Kiss her";
-        choice2Txt.text = "Run Away";
+        int index = Random.Range(0, randomEventTitles.Length - 1);
+        titleText.text = randomEventTitles[index];
+        descriptionText.text = randomEventDescs[index];
+        choice1Txt.text = choice1Texts[index];
+        choice2Txt.text = choice2Texts[index];
         randomEventPanel.SetActive(true);
     }
 
